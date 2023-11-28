83d10e8b57ca2898a415e564bec82325423f1a82
Fill <55672949+filliptm@users.noreply.github.com>
2023-11-28 00:33:41 -0800

    Add files via upload

---
+++ Comfy_Fill-Nodes/__init__.py
@@ -0,0 +1,13 @@
+from .fl_image_randomizer import FL_ImageRandomizer
+
+NODE_CLASS_MAPPINGS = {
+
+    "FL_ImageRandomizer": FL_ImageRandomizer
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+
+    "FL_ImageRandomizer": "FL Image Randomizer"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
---
+++ Comfy_Fill-Nodes/fl_image_randomizer.py
@@ -0,0 +1,52 @@
+import os
+import random
+import numpy as np
+import torch
+from PIL import Image, ImageOps
+
+class FL_ImageRandomizer:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "directory_path": ("STRING", {"default": ""}),
+                "randomize": ("BOOLEAN", {"default": True}),  # Toggle for randomization
+                "run_trigger": ("INT", {"default": 0}),  # Dummy input for caching issue
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "select_image"
+    CATEGORY = "Fill Nodes/Image Processing"  # Adjusted to appear under "Fill Nodes"
+
+    def __init__(self):
+        self.last_index = -1
+
+    def select_image(self, directory_path, randomize, run_trigger):
+        if not directory_path:
+            raise ValueError("Directory path is not provided.")
+
+        images = self.load_images(directory_path)
+        if not images:
+            raise ValueError("No images found in the specified directory.")
+
+        if randomize:
+            selected_image_path = random.choice(images)
+        else:
+            self.last_index = (self.last_index + 1) % len(images)
+            selected_image_path = images[self.last_index]
+
+        image = Image.open(selected_image_path)
+        image = ImageOps.exif_transpose(image)
+        image = image.convert("RGB")
+        image_np = np.array(image).astype(np.float32) / 255.0
+        image_tensor = torch.from_numpy(image_np)[None,]
+
+        return (image_tensor,)
+
+    def load_images(self, directory):
+        supported_formats = ["jpg", "jpeg", "png", "bmp", "gif"]
+        return [os.path.join(directory, f) for f in os.listdir(directory)
+                if os.path.isfile(os.path.join(directory, f)) and f.split('.')[-1].lower() in supported_formats]
+
+# Note: The "run_trigger" parameter is used as a workaround for Comfy UI's caching.
