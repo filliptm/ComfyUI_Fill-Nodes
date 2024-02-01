15115e23d0963d9664b19cb3c3d218ad543be5c6
Fill <55672949+filliptm@users.noreply.github.com>
2024-02-02 08:35:00 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,14 @@
 from .fl_image_randomizer import FL_ImageRandomizer
+from .fl_image_caption_saver import FL_ImageCaptionSaver
 
 NODE_CLASS_MAPPINGS = {
-
-    "FL_ImageRandomizer": FL_ImageRandomizer
+    "FL_ImageRandomizer": FL_ImageRandomizer,
+    "FL_ImageCaptionSaver": FL_ImageCaptionSaver
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-
-    "FL_ImageRandomizer": "FL Image Randomizer"
+    "FL_ImageRandomizer": "FL Image Randomizer",
+    "FL_ImageCaptionSaver": "FL Image Caption Saver"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
