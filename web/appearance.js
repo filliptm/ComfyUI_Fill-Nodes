7f122b8d58ff6af2054f709b49e0fb42ffe33aaf
Fill <55672949+filliptm@users.noreply.github.com>
2024-07-06 00:52:42 -0700

    clean up

---
+++ web/appearance.js
@@ -0,0 +1,43 @@
+import { app } from "../../scripts/app.js";
+
+app.registerExtension({
+    name: "Fill-Nodes.appearance", // Extension name
+    nodeCreated(node) {
+
+        switch (node.comfyClass) {
+            case "FL_ImageRandomizer":
+            case "FL_ImageCaptionSaver":
+            case "FL_ImageDimensionDisplay":
+            case "FL_AudioPreview":
+            case "FL_ImageDurationSync":
+            case "FL_AudioConverter":
+            case "FL_AudioFrameCalculator":
+            case "FL_CodeNode":
+            case "FL_ImagePixelator":
+            case "FL_DirectoryCrawl":
+            case "FL_Ascii":
+            case "FL_Glitch":
+            case "FL_Ripple":
+            case "FL_PixelSort":
+            case "FL_HexagonalPattern":
+            case "FL_NFTGenerator":
+            case "FL_HalftonePattern":
+            case "FL_RandomNumber":
+            case "FL_PromptSelector":
+            case "FL_Shadertoy":
+            case "FL_PixelArtShader":
+            case "FL_InfiniteZoom":
+            case "FL_PaperDrawn":
+            case "FL_ImageNotes":
+            case "FL_ImageCollage":
+            case "FL_InpaintCrop":
+            case "FL_Inpaint_Stitch":
+
+                // node.setSize([200, 58]);
+                node.color = "#16727c";
+                node.bgcolor = "#4F0074";
+
+                break;
+        }
+    }
+});
