8bb80698fe69597e566c85865d350c9760ccb556
Fill <55672949+filliptm@users.noreply.github.com>
2024-07-07 00:38:15 -0700

    cleanup

--- web/appearance.js
+++ web/appearance.js
@@ -3,41 +3,14 @@ import { app } from "../../scripts/app.js";
 app.registerExtension({
     name: "Fill-Nodes.appearance", // Extension name
     nodeCreated(node) {
+        // Check if the node's comfyClass starts with "FL_"
+        if (node.comfyClass.startsWith("FL_")) {
+            // Apply styling
+            node.color = "#16727c";
+            node.bgcolor = "#4F0074";
 
-        switch (node.comfyClass) {
-            case "FL_ImageRandomizer":
-            case "FL_ImageCaptionSaver":
-            case "FL_ImageDimensionDisplay":
-            case "FL_AudioPreview":
-            case "FL_ImageDurationSync":
-            case "FL_AudioConverter":
-            case "FL_AudioFrameCalculator":
-            case "FL_CodeNode":
-            case "FL_ImagePixelator":
-            case "FL_DirectoryCrawl":
-            case "FL_Ascii":
-            case "FL_Glitch":
-            case "FL_Ripple":
-            case "FL_PixelSort":
-            case "FL_HexagonalPattern":
-            case "FL_NFTGenerator":
-            case "FL_HalftonePattern":
-            case "FL_RandomNumber":
-            case "FL_PromptSelector":
-            case "FL_Shadertoy":
-            case "FL_PixelArtShader":
-            case "FL_InfiniteZoom":
-            case "FL_PaperDrawn":
-            case "FL_ImageNotes":
-            case "FL_ImageCollage":
-            case "FL_InpaintCrop":
-            case "FL_Inpaint_Stitch":
-
-                // node.setSize([200, 58]);
-                node.color = "#16727c";
-                node.bgcolor = "#4F0074";
-
-                break;
+            // Uncomment the following line if you want to set a specific size for all FL nodes
+            // node.setSize([200, 58]);
         }
     }
 });
