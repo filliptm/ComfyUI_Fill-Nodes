d2808db2e1ccf14c227c19ae1041d9ae72b183bf
Fillip <fillip.i@hotmail.com>
2024-10-21 05:45:25 -0700

    Fix

--- linux_req_install.sh
+++ linux_req_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update package lists
-sudo apt-get update
+apt-get update
 
 # Install required system libraries for OpenGL
-sudo apt-get install -y libgl1-mesa-glx freeglut3-dev
+apt-get install -y libgl1-mesa-glx freeglut3-dev
 
 # Install Python dependencies
 pip install -r requirements.txt
